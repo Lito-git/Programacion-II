54ba8488830cae7b5a128557f15aaf596277c4a5
Lito-git <tm.lito.salazar@gmail.com>
2025-05-14 14:41:46 -0400

    Subida Examen P2

--- PRUEBA U1 Programacion II/app/src/main/java/cl/mcsalazar/myapplication/modelos/ItemMenu.kt
+++
@@ -1,3 +0,0 @@
-package cl.mcsalazar.myapplication.modelos
-
-data class ItemMenu (val nombre: String, val precio: String)
--- PRUEBA U1 Programacion II/app/src/main/java/cl/mcsalazar/myapplication/modelos/ItemMesa.kt
+++
@@ -1,8 +0,0 @@
-package cl.mcsalazar.myapplication.modelos
-
-class ItemMesa(val itemMenu: ItemMenu, val cantidad: Int) {
-
-    fun calcularSubtotal(): Int{
-        return (itemMenu.precio.toIntOrNull() ?: 0) * cantidad
-    }
-}
